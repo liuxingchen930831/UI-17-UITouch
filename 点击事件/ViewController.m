4339a72ff2df14f9bfc4bccc66e8f3310dce0f47
liuxingchen <liuxingchen0831@gmail.com>
2016-10-18 14:51:59 +0800

    UITouch点击事件：如果想支持多跟手指触摸需要给view 设置>Multiple Touch

---
+++ 点击事件/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  点击事件
+//
+//  Created by liuxingchen on 16/10/18.
+//  Copyright © 2016年 Liuxingchen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
